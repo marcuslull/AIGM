5138b6c5c0488124bedc0a626b593b955157dcb7
Marcus Lull <marcuslull@gmail.com>
2025-02-24 11:12:11 -0800

    Initial commit

---
+++ src/main/java/com/marcuslull/aigm_router/AigmRouterApplication.java
@@ -0,0 +1,13 @@
+package com.marcuslull.aigm_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigmRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AigmRouterApplication.class, args);
+    }
+
+}
